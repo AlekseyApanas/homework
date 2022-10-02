eaee72a483f90fd3fc65ffbbf47a656f4ebc808f
Aleksey <lexaapanas@mail.ru>
2022-10-03 02:01:13 +0300

    [changes]

--- test/home_work_7/paragraph_7/NewMainTest.java
+++ test/home_work_7/paragraph_7/NewMainTest.java
@@ -1,4 +1,28 @@
-import static org.junit.jupiter.api.Assertions.*;
+package home_work_7.paragraph_7;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class NewMainTest {
-  
+    @Test
+    public void test1() {
+        Search search = new Search();
+        search.searchWord("война", "homework/test/home_work_7/paragraph_7/bookTest", "Война.txt", "homework/test/home_work_7/paragraph_7");
+        assertEquals("Война.txt - война - 13", NewMain.readBook("homework/test/home_work_7/paragraph_7"));
+    }
+
+    @Test
+    public void test2() {
+        Search search = new Search();
+        search.searchWord("и", "homework/test/home_work_7/paragraph_7/bookTest", "И.txt", "homework/test/home_work_7/paragraph_7");
+        assertEquals("Война.txt - война - 13\nИ.txt - и - 13", NewMain.readBook("homework/test/home_work_7/paragraph_7"));
+    }
+
+    @Test
+    public void test3() {
+        Search search = new Search();
+        search.searchWord("мир", "homework/test/home_work_7/paragraph_7/bookTest", "Мир.txt", "homework/test/home_work_7/paragraph_7");
+        assertEquals("Война.txt - война - 13\nИ.txt - и - 13\nМир.txt - мир - 13", NewMain.readBook("homework/test/home_work_7/paragraph_7"));
+    }
 }
--- test/home_work_7/paragraph_8/NewMainTest.java
+++ test/home_work_7/paragraph_8/NewMainTest.java
@@ -1,4 +1,43 @@
+package home_work_7.paragraph_8;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class NewMainTest {
-  
+    @Test
+    public void test() {
+        SearchParagraph8 searchParagraph8 = new SearchParagraph8();
+        List<String> strings = searchParagraph8.searchFolder("homework/test/home_work_7/paragraph_7/bookTest");
+        assertEquals(strings,null);
+    }
+    @Test
+    public void test1() {
+        SearchParagraph8 searchParagraph8 = new SearchParagraph8();
+        List<String> strings = searchParagraph8.searchFolder("homework/test/home_work_7/paragraph_7/bookTest");
+        searchParagraph8.searchWord(strings, "homework/test/home_work_7/paragraph_7/bookTest", "Война", "homework/test/home_work_7/paragraph_8");
+        assertEquals("Война.txt - война - 13\nИ.txt - и - 0\nМир.txt - мир - 0",
+                NewMain.readBook("homework/test/home_work_7/paragraph_8"));
+    }
+
+    @Test
+    public void test2() {
+        SearchParagraph8 searchParagraph8 = new SearchParagraph8();
+        List<String> strings = searchParagraph8.searchFolder("homework/test/home_work_7/paragraph_7/bookTest");
+        searchParagraph8.searchWord(strings, "homework/test/home_work_7/paragraph_7/bookTest", "и", "homework/test/home_work_7/paragraph_8");
+        assertEquals("Война.txt - война - 13\nИ.txt - и - 0\nМир.txt - мир - 0\nВойна.txt - война - 0\nИ.txt - и - 13\nМир.txt - мир - 0",
+                NewMain.readBook("homework/test/home_work_7/paragraph_8"));
+    }
+
+    @Test
+    public void test3() {
+        SearchParagraph8 searchParagraph8 = new SearchParagraph8();
+        List<String> strings = searchParagraph8.searchFolder("homework/test/home_work_7/paragraph_7/bookTest");
+        searchParagraph8.searchWord(strings, "homework/test/home_work_7/paragraph_7/bookTest", "мир", "homework/test/home_work_7/paragraph_8");
+        assertEquals("Война.txt - война - 13\nИ.txt - и - 0\nМир.txt - мир - 0\nВойна.txt - война - 0\nИ.txt - и - 13\nМир.txt - мир - 0\nВойна.txt - война - 0\nИ.txt - и - 0\nМир.txt - мир - 13",
+                NewMain.readBook("homework/test/home_work_7/paragraph_8"));
+
+    }
 }
