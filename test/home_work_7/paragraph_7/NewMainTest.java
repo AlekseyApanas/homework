2eb6c2ae9eff7459a8e139ee3e8aa05c3f757356
Aleksey <lexaapanas@mail.ru>
2022-10-03 16:18:33 +0300

    [changes]

--- test/home_work_7/paragraph_7/NewMainTest.java
+++ test/home_work_7/paragraph_7/NewMainTest.java
@@ -2,27 +2,36 @@ package home_work_7.paragraph_7;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class NewMainTest {
     @Test
     public void test1() {
         Search search = new Search();
-        search.searchWord("война", "homework/test/home_work_7/paragraph_7/bookTest", "Война.txt", "homework/test/home_work_7/paragraph_7");
-        assertEquals("Война.txt - война - 13", NewMain.readBook("homework/test/home_work_7/paragraph_7"));
+        search.searchWord("война", "test/home_work_7/paragraph_7/bookTest", "Война.txt", "test/home_work_7/paragraph_7");
+        String[] array = NewMain.readBook("test/home_work_7/paragraph_7").split("\n");
+        String resultText = array[array.length - 1];
+        boolean result = resultText.trim().equals("Война.txt - война - 13");
+        assertTrue(result);
     }
 
     @Test
     public void test2() {
         Search search = new Search();
-        search.searchWord("и", "homework/test/home_work_7/paragraph_7/bookTest", "И.txt", "homework/test/home_work_7/paragraph_7");
-        assertEquals("Война.txt - война - 13\nИ.txt - и - 13", NewMain.readBook("homework/test/home_work_7/paragraph_7"));
+        search.searchWord("и", "test/home_work_7/paragraph_7/bookTest", "И.txt", "test/home_work_7/paragraph_7");
+        String[] array = NewMain.readBook("test/home_work_7/paragraph_7").split("\n");
+        String resultText = array[array.length - 1];
+        boolean result = resultText.trim().equals("И.txt - и - 13");
+        assertTrue(result);
     }
 
     @Test
     public void test3() {
         Search search = new Search();
-        search.searchWord("мир", "homework/test/home_work_7/paragraph_7/bookTest", "Мир.txt", "homework/test/home_work_7/paragraph_7");
-        assertEquals("Война.txt - война - 13\nИ.txt - и - 13\nМир.txt - мир - 13", NewMain.readBook("homework/test/home_work_7/paragraph_7"));
+        search.searchWord("мир", "test/home_work_7/paragraph_7/bookTest", "Мир.txt", "test/home_work_7/paragraph_7");
+        String[] array = NewMain.readBook("test/home_work_7/paragraph_7").split("\n");
+        String resultText = array[array.length - 1];
+        boolean result = resultText.trim().equals("Мир.txt - мир - 13");
+        assertTrue(result);
     }
 }
--- test/home_work_7/paragraph_8/NewMainTest.java
+++ test/home_work_7/paragraph_8/NewMainTest.java
@@ -10,28 +10,33 @@ class NewMainTest {
     @Test
     public void test1() {
         SearchParagraph8 searchParagraph8 = new SearchParagraph8();
-        List<String> strings = searchParagraph8.searchFolder("homework/test/home_work_7/paragraph_7/bookTest");
-        searchParagraph8.searchWord(strings, "homework/test/home_work_7/paragraph_7/bookTest", "Война", "homework/test/home_work_7/paragraph_8");
-        assertEquals("Война.txt - война - 13\nИ.txt - и - 0\nМир.txt - мир - 0",
-                NewMain.readBook("homework/test/home_work_7/paragraph_8"));
+        List<String> strings = searchParagraph8.searchFolder("test/home_work_7/paragraph_7/bookTest");
+        searchParagraph8.searchWord(strings, "test/home_work_7/paragraph_7/bookTest", "война", "test/home_work_7/paragraph_8");
+        String[] array = NewMain.readBook("test/home_work_7/paragraph_8").split("\n");
+        String resultText = array[array.length - 3].trim().concat("\n").concat(array[array.length - 2].trim()).concat("\n").concat(array[array.length - 1]);
+        boolean result = resultText.trim().equals("Война.txt - война - 13\nИ.txt - война - 0\nМир.txt - война - 0");
+        assertTrue(result);
     }
 
     @Test
     public void test2() {
         SearchParagraph8 searchParagraph8 = new SearchParagraph8();
-        List<String> strings = searchParagraph8.searchFolder("homework/test/home_work_7/paragraph_7/bookTest");
-        searchParagraph8.searchWord(strings, "homework/test/home_work_7/paragraph_7/bookTest", "и", "homework/test/home_work_7/paragraph_8");
-        assertEquals("Война.txt - война - 13\nИ.txt - и - 0\nМир.txt - мир - 0\nВойна.txt - война - 0\nИ.txt - и - 13\nМир.txt - мир - 0",
-                NewMain.readBook("homework/test/home_work_7/paragraph_8"));
+        List<String> strings = searchParagraph8.searchFolder("test/home_work_7/paragraph_7/bookTest");
+        searchParagraph8.searchWord(strings, "test/home_work_7/paragraph_7/bookTest", "и", "test/home_work_7/paragraph_8");
+        String[] array = NewMain.readBook("test/home_work_7/paragraph_8").split("\n");
+        String resultText = array[array.length - 3].trim().concat("\n").concat(array[array.length - 2].trim()).concat("\n").concat(array[array.length - 1]);
+        boolean result = resultText.trim().equals("Война.txt - и - 0\nИ.txt - и - 13\nМир.txt - и - 0");
+        assertTrue(result);
     }
 
     @Test
     public void test3() {
         SearchParagraph8 searchParagraph8 = new SearchParagraph8();
-        List<String> strings = searchParagraph8.searchFolder("homework/test/home_work_7/paragraph_7/bookTest");
-        searchParagraph8.searchWord(strings, "homework/test/home_work_7/paragraph_7/bookTest", "мир", "homework/test/home_work_7/paragraph_8");
-        assertEquals("Война.txt - война - 13\nИ.txt - и - 0\nМир.txt - мир - 0\nВойна.txt - война - 0\nИ.txt - и - 13\nМир.txt - мир - 0\nВойна.txt - война - 0\nИ.txt - и - 0\nМир.txt - мир - 13",
-                NewMain.readBook("homework/test/home_work_7/paragraph_8"));
-
+        List<String> strings = searchParagraph8.searchFolder("test/home_work_7/paragraph_7/bookTest");
+        searchParagraph8.searchWord(strings, "test/home_work_7/paragraph_7/bookTest", "мир", "test/home_work_7/paragraph_8");
+        String[] array = NewMain.readBook("test/home_work_7/paragraph_8").split("\n");
+        String resultText = array[array.length - 3].trim().concat("\n").concat(array[array.length - 2].trim()).concat("\n").concat(array[array.length - 1]);
+        boolean result = resultText.trim().equals("Война.txt - мир - 0\nИ.txt - мир - 0\nМир.txt - мир - 13");
+        assertTrue(result);
     }
 }
