e1c45c1dff28b4eb2aa9bec33b697725b2617e38
Aleksey <lexaapanas@mail.ru>
2022-10-03 13:28:12 +0300

    [changes]

--- src/home_work_7/paragraph_8/WorkWithThread.java
+++ src/home_work_7/paragraph_8/WorkWithThread.java
@@ -22,25 +22,25 @@ public class WorkWithThread implements Runnable {
      * Данный метод производит поиск введенного пользователем слова по всем файлам содержащихся в заданной директории
      */
     @Override
-    synchronized public void run() {
-            home_work_7.ISearchEngine easySearch = new home_work_7.search.EasySearch();
-            String cont = String.valueOf(easySearch.search(home_work_7.WorkWithText.getString(this.folderName + "/" + this.fileName), this.word));
-            System.out.println(this.fileName + " " + cont);
-            File file = new File(this.folderNameForSaveResult + "/" + "result.txt");
-            if (file.exists()) {
-                try (FileWriter fileWriter = new FileWriter(file, true)) {
-                    fileWriter.append(this.fileName).append(" - ").append(this.word).append(" - ").append(cont);
-                    fileWriter.append(System.lineSeparator());
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            } else {
-                try (FileWriter fileWriter = new FileWriter(file, true)) {
-                    fileWriter.write(this.fileName + " - " + this.word + " - " + cont);
-                    fileWriter.append(System.lineSeparator());
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
+    public void run() {
+        home_work_7.ISearchEngine easySearch = new home_work_7.search.EasySearch();
+        String cont = String.valueOf(easySearch.search(home_work_7.WorkWithText.getString(this.folderName + "/" + this.fileName), this.word));
+        System.out.println(this.fileName + " " + cont);
+        File file = new File(this.folderNameForSaveResult + "/" + "result.txt");
+        if (file.exists()) {
+            try (FileWriter fileWriter = new FileWriter(file, true)) {
+                fileWriter.append(this.fileName).append(" - ").append(this.word).append(" - ").append(cont);
+                fileWriter.append(System.lineSeparator());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            try (FileWriter fileWriter = new FileWriter(file, true)) {
+                fileWriter.write(this.fileName + " - " + this.word + " - " + cont);
+                fileWriter.append(System.lineSeparator());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
         }
     }
+}
